c6627f45b79c9c24e1bdd91d7c091d5785cf9f59
boy-fc <954506111@qq.com>
2019-10-13 15:24:17 +0800

    登录结构修改以及表单验证优化完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,19 +5,20 @@
         <ValidationObserver tag="form" ref="loginForm">
             <!-- 用户名 -->
             <van-cell-group>
-                <ValidationProvider tag="div" name="手机号" rules="required" v-slot="{ errors }">
-                    <van-field v-model="user.mobile" label='用户名' placeholder="请输入用户名" :error-message="errors[0]"/>
+              <!-- name 提示文本 rules 验证规则 v-slot="{ errors }"显示验证失败消息-->
+                <ValidationProvider name="用户名" rules="required" v-slot="{ errors }">
+                    <van-field required v-model="user.mobile" label='用户名' center placeholder="请输入用户名" :error-message="errors[0]"/>
                 </ValidationProvider>
-                <ValidationProvider tag="div" name="验证码" rules="required" v-slot="{ errors }">
-                    <van-field v-model="user.code" label='验证码' center type='password' placeholder="请输入短信验证码" :error-message="errors[0]">
-                        <van-button slot="button">获取验证码</van-button>
+                <ValidationProvider name="验证码" rules="required" v-slot="{ errors }">
+                    <van-field required v-model="user.code" label='验证码' center type='password' placeholder="请输入短信验证码" :error-message="errors[0]">
+                        <van-button slot="button" type="info" round="true" color="#ededed">获取验证码</van-button>
                     </van-field>
                  </ValidationProvider>
             </van-cell-group>
         </ValidationObserver>
         <!-- 登录按钮 -->
         <div class="login">
-            <van-button type="info" size="large" @click="onLogin" :loading="isLoginLoading">登录</van-button>
+            <van-button type="info" size="large" @click="onLogin">登录</van-button>
         </div>
     </div>
 </template>
@@ -28,7 +29,6 @@ export default {
   name: 'LoginIndex',
   data () {
     return {
-      isLoginLoading: false,
       user: {
         mobile: '15830322153',
         code: '246810'
@@ -37,22 +37,26 @@ export default {
   },
   methods: {
     async onLogin () {
+      this.$toast.loading({
+        duration: 0, // 持续展示 toast
+        forbidClick: true, // 禁用背景点击
+        loadingType: 'spinner',
+        message: '正在登录···'
+      })
       try {
+        // 表单验证
         const isValid = await this.$refs.loginForm.validate()
         if (!isValid) {
           return
         }
-        this.isLoginLoading = true
         // 请求提交表单数据
         await login(this.user)
         this.$toast.success('登录成功')
-        this.isLoginLoading = false
       } catch (err) { // 所有大于等于 400 的状态码都会进入 catch
         if (err.response && err.response.status === 400) {
           this.$toast.fail('登录失败，手机号或验证码错误')
         }
       }
-      this.isLoginLoading = false
     }
   }
 }
@@ -62,7 +66,6 @@ export default {
 .van-cell{
     width: 750px;
     height: 91px;
-    line-height: 91px;
     font-size: 26px;
     padding-left: 40px;
     .van-field__label{
@@ -72,8 +75,14 @@ export default {
         width: 152px;
         height: 46px;
         margin-right: 10px;
-        color: #666
+        .van-button__text{
+          color:#666;
+          font-size: 22px;
+        }
     }
+}
+.van-cell--required::before {
+      left: 30px;
 }
 .login{
     margin-left: 28px;
